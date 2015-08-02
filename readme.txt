ca3708742cb68f2fd15a7615a6593995f9d23990
tricd <tricd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-02 16:10:51 +0000

    Added Version 0.2
    git-svn-id: https://plugins.svn.wordpress.org/zorro/tags/0.2@1211343 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,48 @@
+=== Plugin Name ===
+Contributors: tricd
+Tags: security, spam
+Requires at least: 3.8
+Tested up to: 4.3
+Stable tag: 0.2
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+The Zorro plugin help you to basically secure your WordPress installation.
+
+== Description ==
+
+Attention: If you use WP Super Cache the HTTP-Headers will always overwritten and all header adjustments by WP Zorro will not work.
+
+With Zorro you will enable some basic security settings for your WordPress installation. The current version adds
+the following features:
+
+* removes version indicator from html head and rss feed
+* removes version parameter from enqueued styles and scripts
+* disables text from login screen which can used to brute force usernames
+* removes pingback, super cache and x powered by header
+* adds x-frame-option with SAMEORIGIN policy
+
+More features will hopefully come in future versions
+
+== Installation ==
+
+You can just download to plugin through the WordPress Plugin Repository or install it manually:
+
+1. Upload `zorro.php` to the `/wp-content/plugins/zorro` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Done.
+
+== Frequently Asked Questions ==
+
+= Does this plugin secures me 100%? =
+
+No. It just helps you to add some basic security settings. Depending on your plugins and themes you might be at risk anyway.
+
+
+== Changelog ==
+
+= 0.2 =
+* Fixed some issues and 
+
+= 0.1 =
+* Initial release with some basic settings
---
+++ zorro.php
@@ -0,0 +1,92 @@
+<?php
+/*
+Plugin Name: Zorro - WordPress
+Plugin URI: https://www.tricd.de/zorro/
+Description: Checks the health of your WordPress install
+Author: Tobias Redmann
+Version: 0.1
+Author URI: https://www.tricd.de
+Text Domain: zorro
+*/
+
+class Zorro{
+
+	/**
+	 * Will return an empty string in order to remove WordPress Version from RSS Feed
+	 *
+	 * @return string
+	 */
+	static function removeWordPressVersionFromRSS()
+	{
+		return '';
+	}
+
+	/**
+	 * Removes the ver Parameter from Stylesheet and JS Urls
+	 *
+	 * @param $url
+	 *
+	 * @return mixed
+	 */
+	function removeCssAndJsParameter($url)
+	{
+		if (strpos( $url, 'ver=')) {
+			$url = remove_query_arg('ver', $url);
+		}
+
+		return $url;
+
+	}
+
+	/**
+	 * Removes some parameters from WordPress http response header
+	 *
+	 * @param $headers
+	 *
+	 * @return mixed
+	 */
+	function modifyHeader($headers)
+	{
+
+        $headersToRemove = array(
+            'X-Pingback',
+            'WP-Super-Cache',
+            'X-Powered-By'
+        );
+
+
+        foreach($headersToRemove as $headerToRemove) {
+
+            // try wordpress function
+            if (isset($headers[$headerToRemove])) {
+                unset($headers[$headerToRemove]);
+            }
+
+            // the remove function was not enabled in < 5.3
+            if (version_compare(PHP_VERSION, '5.3.0') >= 0) {
+                header_remove($headerToRemove);
+            } else {
+                header($headerToRemove.':');
+            }
+        }
+
+		// add information
+		$headers['X-Frame-Options'] = 'SAMEORIGIN';
+
+		return $headers;
+
+	}
+
+
+}
+
+
+remove_action('wp_head', 'wp_generator');
+
+add_filter('the_generator', array('Zorro', 'removeWordPressVersionFromRSS'));
+add_filter('style_loader_src', array('Zorro', 'removeCssAndJsParameter'), 9999);
+add_filter('script_loader_src', array('Zorro', 'removeCssAndJsParameter'), 9999);
+add_filter('wp_headers', array('Zorro', 'modifyHeader'), 9999);
+
+// add_filter('xmlrpc_enabled', '__return_false' );
+add_filter('login_errors', create_function('$a', "return null;"));
